1cf49f9a13dec5cf3c3a98fd1d64e5ee180d0c3d
thisiskeshavyadavv@gmail.com <Keshav yadav>
2024-08-29 12:22:35 +0530

    stage 1 create initialize functino initiateoffer and createpeerconnection

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const debug = require('debug')('development:app.js');
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+
+// socket .io setup
+
+const http = require('http');
+const socketIO = require('socket.io');
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket)=>{
+    console.log("user is connected!!!");
+})
+
+app.get('/', function(req, res){
+    res.render('index');
+})
+
+app.listen(3000, function(req,res){
+    console.log('listening on port 3000');
+})
